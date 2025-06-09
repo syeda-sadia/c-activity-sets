cf6672339dbeedf4e91a8cf3685cc4eae9a4ce15
syeda-sadia <syedasadia.tabassum@reva.edu.in>
2025-06-09 11:00:14 +0530

    Update problem03.c

--- set01/problem03.c
+++ set01/problem03.c
@@ -1,1 +1,26 @@
 //3. Write a C program to add two numbers using pass by value
+#include <stdio.h>
+int input();
+int add(int a, int b);
+void output(int a, int b, int sum);
+int main() {
+    int a,b,sum;
+    a=input();
+    b=input();
+    sum = add(a,b);
+    output(a,b,sum);
+    return 0;
+}
+int input(){
+    int x;
+    printf("Enter the number: ");
+    scanf("%d",&x);
+    return x;
+}
+int add(int a, int b) {
+  int sum = a + b;
+  return sum;
+}
+void output(int a, int b, int sum){
+    printf("sum of %d and %d is %d",a,b,sum);
+}
